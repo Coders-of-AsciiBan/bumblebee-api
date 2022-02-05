20047fb216af60ae28d9832b81cffee2a54858c2
Sasirekha Kambhampaty <ksasirekha@gmail.com>
2022-02-05 16:20:53 +0000

    init commit

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-aws s3 cp s3://secrets-sasirekha3/api-pdiot/secrets.js secrets.js
+aws s3 cp s3://secrets-sasirekha3/bumblebee-api/secrets.js secrets.js
 
 zip -r lambda.zip .
 
-aws s3 cp lambda.zip s3://api-pdiot/${GITHUB_RUN_ID}/
+aws s3 cp lambda.zip s3://bumblebee-api/${GITHUB_RUN_ID}/
 
 {
-  aws cloudformation describe-stacks --stack-name api-pdiot
+  aws cloudformation describe-stacks --stack-name bumblebee-api
 
-  aws cloudformation update-stack --stack-name api-pdiot \
+  aws cloudformation update-stack --stack-name bumblebee-api \
       --template-body file://cfn.yml \
       --parameters ParameterKey=BuildId,ParameterValue=${GITHUB_RUN_ID} \
       --capabilities CAPABILITY_NAMED_IAM
 } || {
-  aws cloudformation create-stack --stack-name api-pdiot \
+  aws cloudformation create-stack --stack-name bumblebee-api \
     --template-body file://cfn.yml \
     --parameters ParameterKey=BuildId,ParameterValue=${GITHUB_RUN_ID} \
     --capabilities CAPABILITY_NAMED_IAM
