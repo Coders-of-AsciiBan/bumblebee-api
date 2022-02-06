5badbb5b0811128f9ffc07f130c07b50c2ebc86e
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2022-02-06 01:47:28 +0000

    add env variable port

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const pkg = require('@prisma/client');
-
+const dotenv = require('dotenv');
 const express = require('express');
 const cors = require('cors');
 const app = express();
+
+dotenv.config({ path: './.env' });
 
 const { PrismaClient } = pkg;
 
@@ -20,41 +22,39 @@ app.get('/favicon.ico', function (req, res) {
 
 module.exports = app;
 
-app.get('/', function(req,res){
-    res.status(200).send("Server ready at: http://localhost:3000 🚀");
+app.get('/', function (req, res) {
+  res.status(200).send('Server ready at: http://localhost:3000 🚀');
 });
 
-app.get('/fetchCategories',  async(req, res) => {
+app.get('/fetchCategories', async (req, res) => {
   //Code to fetch the different categories from the db
-  try{
+  try {
     const categories = await prisma.category.findMany();
     console.log(categories);
     res.status(200).send({ body: categories, message: 'Success!' });
-  }
-  catch(error){
-      console.log(error);
-      res.status(500).send("Internal Server Error :(");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Internal Server Error :(');
   }
 });
 
-app.get('/game', async (req, res) =>{
+app.get('/game', async (req, res) => {
   //Code to extract the data from the db based on the relevant category
-try{
+  try {
     const products = await prisma.product.findMany({
-        select : {
-            id: true,
-            name: true,
-            price: true,
-            image: true,
-            url: true
-        }
+      select: {
+        id: true,
+        name: true,
+        price: true,
+        image: true,
+        url: true,
+      },
     });
-    res.status(200).send({body: products, message: "Success!!"});
-}
-catch(error){
+    res.status(200).send({ body: products, message: 'Success!!' });
+  } catch (error) {
     console.log(error);
-    res.status(404).send("Data Not Found!");
-}
+    res.status(404).send('Data Not Found!');
+  }
 });
 
 // Object format for submissionData
@@ -76,89 +76,88 @@ app.get('/gameScore', async (req, res) => {
 
   //Local testing implementation for userData - comment it out while commiting
   var userData = {
-      "userName": "abc",
-      "email": "abc@gmail.com",
-      "submissionData": {
-          "score": 4,
-          "gameItems": [{
-            "productId": "P60515662",
-            "guessedPrice": 11
-          },
-          {
-            "productId": "P60371466",
-            "guessedPrice": 25
-          },
-          {
-            "productId": "P60370104",
-            "guessedPrice": 30
-          },
-          {
-            "productId": "P22491941",
-            "guessedPrice": 60
-          },
-          {
-            "productId": "P60267024",
-            "guessedPrice": 24
-          },
-        ]
-      }
-};
-var { userName, email, submissionData} = userData;
-try{
+    userName: 'abc',
+    email: 'abc@gmail.com',
+    submissionData: {
+      score: 4,
+      gameItems: [
+        {
+          productId: 'P60515662',
+          guessedPrice: 11,
+        },
+        {
+          productId: 'P60371466',
+          guessedPrice: 25,
+        },
+        {
+          productId: 'P60370104',
+          guessedPrice: 30,
+        },
+        {
+          productId: 'P22491941',
+          guessedPrice: 60,
+        },
+        {
+          productId: 'P60267024',
+          guessedPrice: 24,
+        },
+      ],
+    },
+  };
+  var { userName, email, submissionData } = userData;
+  try {
     await prisma.submission.create({
-        data:{
-            user:{
-                connectOrCreate:{
-                    create:{
-                        email:email,
-                        username:userName
-                    },
-                    where:{
-                        email:email
-                    }
-                }
+      data: {
+        user: {
+          connectOrCreate: {
+            create: {
+              email: email,
+              username: userName,
             },
-            score:submissionData.score,
-            guesses:{
-              createMany:{
-                  data:submissionData.gameItems.map(item=>{
-                      return{...item}
-                  })
-              }
-            }
-        }
-      })
-      res.status(200).send({ message: 'Data recorded!' });
-}
-catch(error){
+            where: {
+              email: email,
+            },
+          },
+        },
+        score: submissionData.score,
+        guesses: {
+          createMany: {
+            data: submissionData.gameItems.map((item) => {
+              return { ...item };
+            }),
+          },
+        },
+      },
+    });
+    res.status(200).send({ message: 'Data recorded!' });
+  } catch (error) {
     console.log(error);
-    res.status(500).send("Internal Server Error");
-}
-  //Code to store this data into the db 
+    res.status(500).send('Internal Server Error');
+  }
+  //Code to store this data into the db
 });
 
 app.get('/leaderboard', async (req, res) => {
   //Code to fetch all the usernames and scores from the db and sending it to the front end
-try{
+  try {
     const leaderboardData = await prisma.user.findMany({
-        select:{
-            username: true,
-            submissions:{
-                select:{
-                    score:true
-                }
-            }
-            }
+      select: {
+        username: true,
+        submissions: {
+          select: {
+            score: true,
+          },
+        },
+      },
     });
-    res.status(200).send({"body":leaderboardData, "message": "Data extracted successfully!"});
-}
-catch(error){
+    res.status(200).send({ body: leaderboardData, message: 'Data extracted successfully!' });
+  } catch (error) {
     console.log(error);
-    res.status(500).send("Oops...Internal Server Error! Please try again later!");
-}
+    res.status(500).send('Oops...Internal Server Error! Please try again later!');
+  }
 });
 
-const server = app.listen(3000, () =>
+app.listen(process.env.PORT, () =>
   console.log(`
-Server ready at: http://localhost:300 🚀 `),
+Server ready at: http://localhost:${process.env.PORT} 🚀 `)
 );
